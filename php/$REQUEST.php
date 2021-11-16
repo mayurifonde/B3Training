d5212c133d187ec509347da06012cfad761de7b0
mayurifonde <92967910+mayurifonde@users.noreply.github.com>
2021-11-16 11:08:05 +0530

    Add files via upload

---
+++ php/$REQUEST.php
@@ -0,0 +1,26 @@
+<html>
+    <body>
+        <form method="post" action ="<?php 
+        echo $_SERVER['PHP_SELF'];?>">
+
+        Name:<input type="text" name="fname">
+        <input type="submit">
+</form>
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST")
+
+
+{
+$name=$_REQUEST['fname'];    //same for the $_POST
+if(empty($name))
+{
+    echo "Name is empty";
+
+}
+else{
+    echo $name;
+}
+}
+?>
+</body>
+</html>
